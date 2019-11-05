6d43d4ce62675cb7eab7d986aee8abd101fa82ec
Alexey Panchenko <panchenko@farpost.com>
2019-11-05 16:24:51 +1000

    add tests for custom type in OnCreateVisitor

--- stater/src/test/java/ru/alexpanchenko/stater/plugin/visitors/OnCreateVisitorTest.java
+++ stater/src/test/java/ru/alexpanchenko/stater/plugin/visitors/OnCreateVisitorTest.java
@@ -9,11 +9,15 @@ import ru.alexpanchenko.stater.plugin.model.StateType;
 import ru.alexpanchenko.stater.plugin.utils.Descriptors;
 import ru.alexpanchenko.stater.plugin.utils.Methods;
 import ru.alexpanchenko.stater.plugin.utils.Types;
+import stater.org.objectweb.asm.Label;
 import stater.org.objectweb.asm.MethodVisitor;
 import stater.org.objectweb.asm.Opcodes;
 import stater.org.objectweb.asm.Type;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class OnCreateVisitorTest {
@@ -32,6 +36,34 @@ public class OnCreateVisitorTest {
 
     OnCreateVisitor onCreateVisitor = new OnCreateVisitor(mockMethodVisitor, fieldStorage);
     onCreateVisitor.visitCode();
+  }
+
+  @Test
+  public void testBaseFlow() {
+    String fieldName = "field1";
+    String fieldDescriptor = Descriptors.INT;
+    String fieldOwner = Descriptors.INT;
+    StateType fieldType = StateType.INT;
+    MethodVisitor mockMethodVisitor = mock(MethodVisitor.class);
+    SaverField field = new SaverField(fieldName, fieldDescriptor, null, fieldOwner, fieldType);
+    fieldStorage.add(field);
+
+    OnCreateVisitor onCreateVisitor = new OnCreateVisitor(mockMethodVisitor, fieldStorage);
+    onCreateVisitor.visitCode();
+
+    verify(mockMethodVisitor).visitCode();
+    verify(mockMethodVisitor, times(2)).visitVarInsn(Opcodes.ALOAD, 1);
+    verify(mockMethodVisitor).visitJumpInsn(eq(Opcodes.IFNULL), any(Label.class));
+    verify(mockMethodVisitor).visitVarInsn(Opcodes.ALOAD, 0);
+    verify(mockMethodVisitor).visitLdcInsn(field.key);
+    verify(mockMethodVisitor).visitMethodInsn(
+      Opcodes.INVOKEVIRTUAL,
+      Types.BUNDLE,
+      Methods.Get.INT,
+      "(" + Descriptors.STRING + ")" + Descriptors.INT,
+      false
+    );
+    verify(mockMethodVisitor).visitFieldInsn(Opcodes.PUTFIELD, field.owner, field.name, field.descriptor);
   }
 
   @Test
@@ -73,5 +105,60 @@ public class OnCreateVisitorTest {
     onCreateVisitor.visitCode();
 
     verify(mockMethodVisitor).visitTypeInsn(Opcodes.CHECKCAST, Type.getType(field.descriptor).getInternalName());
+  }
+
+  @Test
+  public void testCustomField() {
+    String fieldName = "field1";
+    String fieldDescriptor = Descriptors.OBJECT;
+    String fieldOwner = Descriptors.OBJECT;
+    StateType fieldType = StateType.CUSTOM;
+    MethodVisitor mockMethodVisitor = mock(MethodVisitor.class);
+    SaverField field = new SaverField(fieldName, fieldDescriptor, null, fieldOwner, fieldType);
+    fieldStorage.add(field);
+
+    OnCreateVisitor onCreateVisitor = new OnCreateVisitor(mockMethodVisitor, fieldStorage);
+    onCreateVisitor.visitCode();
+
+    verify(mockMethodVisitor).visitLdcInsn(Type.getType(field.descriptor));
+    verify(mockMethodVisitor).visitMethodInsn(
+      Opcodes.INVOKESTATIC,
+      Types.SERIALIZER,
+      Methods.DESERIALIZE,
+      Descriptors.SERIALIZER_DESERIALIZE,
+      false
+    );
+    verify(mockMethodVisitor).visitTypeInsn(Opcodes.CHECKCAST, Type.getType(field.descriptor).getInternalName());
+  }
+
+  @Test
+  public void testCustomTypedField() {
+    String fieldName = "field1";
+    String fieldDescriptor = Descriptors.LIST;
+    String fieldSignature = "L" + Types.LIST + "<" + Descriptors.OBJECT + ">;";
+    String fieldOwner = Descriptors.OBJECT;
+    StateType fieldType = StateType.CUSTOM;
+    MethodVisitor mockMethodVisitor = mock(MethodVisitor.class);
+    SaverField field = new SaverField(fieldName, fieldDescriptor, fieldSignature, fieldOwner, fieldType);
+    fieldStorage.add(field);
+
+    OnCreateVisitor onCreateVisitor = new OnCreateVisitor(mockMethodVisitor, fieldStorage);
+    onCreateVisitor.visitCode();
+
+    verify(mockMethodVisitor).visitInsn(Opcodes.ICONST_2);
+    verify(mockMethodVisitor).visitTypeInsn(Opcodes.ANEWARRAY, Types.CLASS);
+    verify(mockMethodVisitor, times(2)).visitInsn(Opcodes.DUP);
+    verify(mockMethodVisitor).visitInsn(Opcodes.ICONST_0);
+    verify(mockMethodVisitor).visitInsn(Opcodes.ICONST_1);
+    verify(mockMethodVisitor).visitLdcInsn(Type.getObjectType(Types.LIST));
+    verify(mockMethodVisitor).visitLdcInsn(Type.getObjectType(Types.OBJECT));
+    verify(mockMethodVisitor, times(2)).visitInsn(Opcodes.AASTORE);
+    verify(mockMethodVisitor).visitMethodInsn(
+      Opcodes.INVOKESTATIC,
+      Types.SERIALIZER,
+      Methods.DESERIALIZE_TYPED,
+      Descriptors.SERIALIZER_DESERIALIZE_TYPED,
+      false
+    );
   }
 }
